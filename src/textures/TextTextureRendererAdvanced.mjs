94cb0ea8619384946ef1e2a7a0b78475607a714b
sandeep-vedam <sandeepsharma.vedam@outlook.com>
2025-02-18 12:48:12 +0530

    Updated proper file extensions in Text Texture files

--- src/textures/TextTextureRendererAdvanced.mjs
+++ src/textures/TextTextureRendererAdvanced.mjs
@@ -19,7 +19,7 @@
 
 import StageUtils from "../tree/StageUtils.mjs";
 import Utils from "../tree/Utils.mjs";
-import { getFontSetting, isSpace, measureText, tokenizeString } from "./TextTextureRendererUtils.mjs";
+import { getFontSetting, isSpace, measureText, tokenizeString } from "./TextTextureRendererUtils.mts";
 
 export default class TextTextureRendererAdvanced {
 
